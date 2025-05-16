4d5a9c9a52b15ec999d24e9cac137ba027262d16
EuNetu <euneto2501@gmail.com>
2025-05-16 00:26:16 -0300

    feat: adiciona validações

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,15 +1,38 @@
-import fastifyCors from "@fastify/cors";
-import fastify from "fastify";
+import fastifyCors from '@fastify/cors'
+import fastify from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
-const server = fastify();
+const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.validation })
+  }
+  console.error(error)
+  reply.status(500).send({ message: 'Internal server error' })
+})
 
 server.register(fastifyCors, {
-    origin: "*",
-});
+  origin: '*',
+})
 
-server.listen({
+server.register(uploadImageRoute)
+
+server
+  .listen({
     port: 3333,
-    host: "0.0.0.0"
-}).then(() => {
-    console.log("HTTP server running on http://localhost:3333");
-});
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
