f6819d378b5b33910596e7e5cb229042f629f38e
EuNetu <euneto2501@gmail.com>
2025-05-18 00:17:36 -0300

    chore: cria modelo de uploads

---
+++ src/infra/db/schemas/uploads.ts
@@ -0,0 +1,11 @@
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { randomUUID } from 'node:crypto'
+
+export const uploads = pgTable('uploads', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => randomUUID()),
+  name: text('name').notNull(),
+  remoteKey: text('remote_key').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+})
