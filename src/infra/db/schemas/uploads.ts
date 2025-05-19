095c8d98111d397cee252b550b233c2ca3e2a320
EuNetu <euneto2501@gmail.com>
2025-05-19 01:44:58 -0300

    chore: cria objeto de acesso a manipulação da tabela uploads

--- src/infra/db/schemas/uploads.ts
+++ src/infra/db/schemas/uploads.ts
@@ -1,11 +1,12 @@
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
-import { randomUUID } from 'node:crypto'
+import { uuidv7 } from 'uuidv7'
 
 export const uploads = pgTable('uploads', {
   id: text('id')
     .primaryKey()
-    .$defaultFn(() => randomUUID()),
+    .$defaultFn(() => uuidv7()),
   name: text('name').notNull(),
-  remoteKey: text('remote_key').notNull(),
+  remoteKey: text('remote_key').notNull().unique(),
+  remoteUrl: text('remote_url').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 })
