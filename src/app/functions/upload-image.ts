3bc6efaa823667e3fa213824a6b1196835264610
EuNetu <euneto2501@gmail.com>
2025-05-20 23:48:36 -0300

    chore: cria função para inserir dados do arquivo no banco

---
+++ src/app/functions/upload-image.ts
@@ -0,0 +1,31 @@
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { type Either, makeLeft, makeRight } from '@/infra/shared/either'
+import { Readable } from 'node:stream'
+import { z } from 'zod'
+import { InvalidFileFormat } from './errors/invalid-file-format'
+
+const uploadImageInput = z.object({
+  fileName: z.string(),
+  contentType: z.string(),
+  contentStream: z.instanceof(Readable),
+})
+
+type UploadImageInput = z.input<typeof uploadImageInput>
+
+const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp']
+
+export async function uploadImage(input: UploadImageInput) : Promise<Either<InvalidFileFormat, { url: string }>> {
+  const { contentStream, contentType, fileName } = uploadImageInput.parse(input)
+
+  if (!allowedMimeTypes.includes(contentType)) {
+    return makeLeft(new InvalidFileFormat())
+  }
+
+  await db.insert(schema.uploads).values({
+    name: fileName,
+    remoteKey: fileName,
+    remoteUrl: '',
+  })
+  return makeRight({ url: '' })
+}
